ebe3604872c0e210380d8d4a4fb8ca51a9cb5865
hackerAbdul <abdulkadir.q12@gmail.com>
2022-12-19 19:56:11 +0000

    Setting up github for weather app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +14,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Visit React Website</a>
       </header>
     </div>
   );
