f9c706106344a1750863eda96ba8acd4755b0b8b
hackerAbdul <abdulkadir.q12@gmail.com>
2022-12-26 21:14:59 +0000

    starting on making some css changes to the weather app

--- src/App.js
+++ src/App.js
@@ -5,15 +5,36 @@ import './App.css';
 function App() {
 
 
+  
+  //const url = `https://api.openweathermap.org/data/2.5/weather?q=Birmingham,uk&appid=9c635a90c6d2d9352ee769c425d53ab6`
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Visit React Website</a>
-      </header>
+    <div className="app">
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Birmingham</p>
+          </div>
+          <div className='temp'>
+            <h1>5°C</h1>
+          </div>
+          <div className='description'>
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className='bottom'>
+          <div className='feels'>
+            <p>0°c</p>
+          </div>
+          <div className='humidity'>
+            <p>84%</p>
+          </div>
+          <div className='wind'>
+            <p>10mph</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
